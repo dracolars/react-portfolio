2103c4afeeff2276dcb7768e43a6ade193683ed2
dracolars <dracolars@users.noreply.github.com>
2023-02-22 18:28:26 -0600

    form endpoint updated!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,42 +1,23 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { useForm } from '@formcarry/react'
 
 const Contact = () => {
-
-  const[status, setStatus] = useState("Ready to send");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus('Sending...')
-
-    const {name, email, message} = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    
-    let response = await fetch('${window.location.hostname}:5000/contact', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
+  let formstatus = "form ready";
+    const {state, submit} = useForm({
+      id: 'FyAaAv2e5r'
     });
 
-    let result = await response.json();
-    console.log(result.status);
-    if (result.status === "Message Sent") {
-      name.value = "";
-      email.value = ""; 
-      message.value = "";
-      setStatus('Message sent. Thanks!')
-    };
-  };
+    //success mesage
+    if (state.submitted){
+      var form = document.getElementById('portfolio-contact');
+      var allformcontrols = form.elements;
+      allformcontrols[0].value = ""; allformcontrols[1].value = ""; allformcontrols[2].value = "";
+      formstatus = "form received. thanks!"
+    }
 
   return (
     <div name='contact' className='w-full h-screen bg-[#efeeed] flex justify-center items-center p-4'>
-      {/* the free backend we used previously: https://getform.io/f/b154eeb6-f032-4311-9edd-2d7ede7fb146 */}
-        <form id='portfolio-contact' onSubmit={handleSubmit} className='flex flex-col max-w-[600px] w-full'>
+        <form id='portfolio-contact' onSubmit={submit} className='flex flex-col max-w-[600px] w-full'>
             <div className='py-4'>
                 <p className='pagetitle'>CONTACT</p>
                 <p className='text-gray-700 py-6'>Submit the form below or shoot me an email - 
@@ -46,11 +27,11 @@ const Contact = () => {
             <input className='p-2 bg-[#ffffff]' type="text" placeholder='Name' name="name" id="name" required/>
             <input className='my-4 p-2 bg-[#ffffff]' type="email" placeholder='Email' name="email" id="email" required/>
             <textarea className='bg-[#ffffff] p-2' type="text" placeholder='Message' name="message" id="message" rows='6' required/>
-            <button type="submit" className='text-gray-100 bg-[#6a6a68] border-2 hover:bg-[#979795] hover:border-[#ffffff] px-4 py-3 my-8 mx-auto items-center'> Let's Collaborate</button>
-            <p className='text-[#a2a2a2] text-center text-sm'>{status}</p>
+            <button type="submit" className='text-gray-100 bg-[#6a6a68] border-2 hover:bg-[#979795] hover:border-[#ffffff] px-4 py-3 mt-8 mb-2 mx-auto items-center'> Let's Collaborate</button>
+            <p className='text-[#c0c0c0] text-center'>{formstatus}</p>
         </form>
     </div>
-  )
+   )
 }
 
 export default Contact
