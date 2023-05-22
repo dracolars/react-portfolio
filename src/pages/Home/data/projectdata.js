37a23c9f2c14ba80c605bf036470eedbfe7af898
dracolars <dracolars@users.noreply.github.com>
2023-05-22 17:33:26 -0500

    added matching game project

--- src/pages/Home/data/projectdata.js
+++ src/pages/Home/data/projectdata.js
@@ -2,6 +2,7 @@ import SpootifyImage from "../../../assets/Spootify-Cover.png";
 import PianoPlayer from "../../../assets/Piano-player-Cover.png";
 import EyemedCover3 from "../../../assets/EyemedCover9.png";
 import GA4 from "../../../assets/GACover2.png";
+import MatchingGame from "../../../assets/Memory-Game-Cover.png";
 
 export const projectData = [
   {
@@ -17,6 +18,13 @@ export const projectData = [
     detailsLink: "",
     codeLink: "https://github.com/dracolars/piano-player-js",
     image: PianoPlayer,
+  },
+  {
+    title: "Memory Matching Game (React-Redux)",
+    demoLink: "https://memory-game-matching.netlify.app/",
+    detailsLink: "",
+    codeLink: "https://github.com/dracolars/memory-game",
+    image: MatchingGame,
   },
   {
     title: "Insurance Claim Automation (Python)",
